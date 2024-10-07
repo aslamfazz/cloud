58e95a4ba03edeb17ea945e0ffdf813b1343094f
aslamfazz <133159720+aslamfazz@users.noreply.github.com>
2024-10-07 16:44:35 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
-
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # or another version that is compatible
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
-
 }
-
