bfbd047540ac180e0bc3c5e06e0089c3f5bede5a
aslamfazz <133159720+aslamfazz@users.noreply.github.com>
2024-10-07 16:27:46 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 resource "azurerm_resource_group" "prod1" {
  
   name     = "prod11"
