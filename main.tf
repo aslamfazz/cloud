6d6fbe961af427f16ffd7fd2c3770e757e582837
aslamfazz <133159720+aslamfazz@users.noreply.github.com>
2024-10-07 17:04:11 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0" # or another version that is compatible
+      version = "~> 3.0"  # Specify a compatible version if needed
     }
   }
 }
 
 provider "azurerm" {
-  zurerm.azsubscription = azurerm
+  features {}
 }
